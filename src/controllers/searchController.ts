1b4607218094e3aede904d14f09a9999c7b31f35
Clara Armani <c.armani@aluno.ifsp.edu.br>
2026-03-25 08:41:54 -0300

    projeto finalizado

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet'
+import { createMenuObejct } from '../helpers/createMenuObject';
 
 export const search = (req: Request, res: Response) => {
-    res.send('Buscando...');
+    let query: string = req.query.q as string
+    if(!query) {
+        res.redirect('/')
+        return
+    }
+    let list = Pet.getFromName(query)
+    res.render('pages/pages', {
+        menu: createMenuObejct(''),
+        list,
+        query
+    });
 };
