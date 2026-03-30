acd2a03988d349c190a300daa2692f6f6b48182f
Clara Armani <c.armani@aluno.ifsp.edu.br>
2026-03-30 08:11:37 -0300

    finalisando projeto

--- src/models/pet.ts
+++ src/models/pet.ts
@@ -137,32 +137,12 @@ export const Pet = {
     },
 
     getFromType: (type: PetType): Pet[] => {
-        //  return data.filter(item => {
-        // primeira froma
-        // if(item.type === type) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-        //segunda forma
-        //     return (item.type === type)
-        // })
-        //terceira forma
-        return data.filter(item => item.type === type);
+        return data.filter(item => item.type === type)
     },
 
     getFromName: (name: string): Pet[] => {
-        //primeira forma
-        //return data.filter(item => {
-        // if(item.name.indexOf(name) > -1) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-        //segunda forma
-        //   return (item.name.indexOf(name) > -1);
-        // })
-        return data.filter(item => item.name.toLocaleLowerCase().indexOf(name.toLocaleLowerCase()) > -1
+        return data.filter(item => 
+            item.name.toLowerCase().indexOf(name.toLowerCase()) > -1
     );
     }
 }
